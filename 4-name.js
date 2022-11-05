c111dc01852dc63a75077218787642eddd1b1daf
seyha30 <57175099+seyha30@users.noreply.github.com>
2022-11-05 22:39:28 +0700

    first

---
+++ 4-name.js
@@ -0,0 +1,7 @@
+// local
+const secret = 'SUPER SECRET'
+// shared 
+const john = 'john'
+const peter = 'peter'
+//console.log(module)
+module.exports =  { john , peter}
---
+++ app.js
@@ -0,0 +1,28 @@
+
+//  GLOBALS - No window
+// __dirname - path to current directory
+// __filename - file name
+// require   - function to use modules(CommonJS)
+// module    - info about current module (file)
+// process   - info about env where the program is being executed
+
+const amount = 90
+if(amount < 10) {
+    console.log('small number')
+}else {
+    console.log('large number')
+}
+console.log(`hey it's my first node app`)
+console.log(__dirname)
+setInterval(() => {
+console.log('Hello World !')
+},1000)
+
+const names = require('./4-name')
+const sayHi = require('./5-utils')
+
+console.log(names)
+sayHi('susan')
+sayHi('john')
+sayHi('peter')
+sayHi(names.john)
