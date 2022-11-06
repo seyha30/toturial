7af42343569ce7d73a419ad866399148c6f2dd89
seyha30 <57175099+seyha30@users.noreply.github.com>
2022-11-06 22:49:41 +0700

    mmmm

--- app.js
+++ app.js
@@ -1,14 +1,5 @@
-const http = require('http')
-const server = http.createServer((reg,res) => {
-
-if(reg.url === '/') {
-    res.end('Welcome to our home page') 
-}
-if(reg.url === '/about') {
-
-}
-
-res.end(`<h1>Oops</h1>`)
-
-})
-server.listen(6000) 
+const _ = require('lodash')
+const items = [1,[2,[3,[4,[5]]]]]
+const newItems = _.flattenDeep(items)
+console.log(newItems)
+console.log('123')
